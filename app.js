61166de2317f453af9d0cbf5efe37ca84e04bb1c
timothyxvo <timothyvo@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 15:22:03 -0800

    please

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ $(function(){
                 for(var key in all_tracks)
                 {
                     if(all_tracks.hasOwnProperty(key)) {
-                        $("#songList").append("<p>"+all_tracks[key]["title"]+"</p>");
+                        $("#songList").append("<option>"+all_tracks[key]["title"]+"</option>");
                     }
                    
                 }
