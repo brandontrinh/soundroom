90deae752c0f02586dc5ace45534fd3fd36ba744
calvwng <calvwng@gmail.com>
2014-11-08 04:09:03 -0800

    Adding embedded player for Top 10 playlist
    Added soundcloud sdk reference in index.html; Added player.js for
    retrieving embedded player as JSON and putting it in the 'player' div

---
+++ js/player.js
@@ -0,0 +1,16 @@
+// initialize client with app credentials
+SC.initialize({
+  client_id: '103e05240820b5d0d7d5220ba773612c',
+});
+
+
+$.getJSON('http://soundcloud.com/oembed?callback=?',
+    {
+         format: 'js', 
+         auto_play: true,
+         url: 'https://soundcloud.com/lovekyle/fruit-snacks-cups-of-patron', iframe: true},
+         function(data) {
+             // Put html content returned into 'player' div
+             $('#player').html(data['html'])
+         }
+)
