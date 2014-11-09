d3ffa04770950bcf0633702d0dc800808dae9424
Brandon Trinh <bhtrinh@uci.edu>
2014-11-08 16:06:29 -0800

    itred

--- js/init.js
+++ js/init.js
@@ -59,7 +59,6 @@
     		var TestObject = Parse.Object.extend("TestObject");
 			var testObject = new TestObject();
 			testObject.save({foo: "bar"}).then(function(object) {
-			  alert("yay! it worked");
 			});
 		});
 	});
