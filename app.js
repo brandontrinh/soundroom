e232550f0deee8e2241a5d059a4cce4aeb23add0
timothyxvo <timothyvo@Timothys-MacBook-Pro.local>
2014-11-09 01:05:01 -0800

    Revert bde7c8b..d213290
    This rolls back to commit bde7c8b459bfd7650fc47a05b90f51159419a2b1.

--- app.js
+++ app.js
@@ -55,8 +55,9 @@ $(function(){
         Parse.initialize("ovwXFPTmzVJfebpzH1iJLLdJsKtMEqn9jD3cmBZW", "q7n9xQOnhCtcGzKUgGfW196IyuFqYGJnLMCZeWGZ");
         var Track = Parse.Object.extend("Track");
 
-        $("#top1").src = "https://w.soundcloud.com/player/?url=https://soundcloud.com/giraffage/close-2-me?show_artwork=false";
-        SC.Widget("top1");
+        var testSong = "<iframe id=&quot;rPlayer1&quot; width=&quot;100%&quot; height=&quot;95px&quot; src=&quot;https://w.soundcloud.com/player/?url=https://soundcloud.com/giraffage/close-2-me?show_artwork=false&quot;></iframe>";
+        $("#topEmbedded").html(testSong);
+        //$("#topEmbedded").append("testtext");
 
         $("#start").click(function(){
             $("#top").hide(500);
