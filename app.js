ef37cc81883ad2cc8c5108d305aac0eb8e707a02
Brandon Trinh <bhtrinh@uci.edu>
2014-11-09 00:02:03 -0800

    testtext

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ $(function(){
         var Track = Parse.Object.extend("Track");
 
         var testSong = "<iframe id=&quot;rPlayer1&quot; width=&quot;100%&quot; height=&quot;95px&quot; src=&quot;https://w.soundcloud.com/player/?url=https://soundcloud.com/giraffage/close-2-me?show_artwork=false&quot;></iframe>";
-        $("#topEmbedded").append(testSong);
+        //$("#topEmbedded").append(testSong);
+        $("#topEmbedded").append("testtext");
 
         $("#start").click(function(){
             $("#top").hide(500);
